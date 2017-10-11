2a13391e4585b2c6dba22c91583ab6e1b4d4b1cc
Ricardo Dantas <ricardodantas@gmail.com>
2017-10-11 23:41:36 +0100

    update package version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { checkSlotsAvailability } from './lib/checkSlotsAvailability';
 import { Types, Categories, Subcategories } from './lib/interfaces';
+import { checkSlotsAvailability } from './lib/checkSlotsAvailability';
 
 export default {
   checkSlotsAvailability,
